3678d1a66cc7ce2a04df6107d8e8e3ee26931203
wiktoria250001 <149382337+wiktoria250001@users.noreply.github.com>
2024-05-19 19:59:41 +0200

    update home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Home.css";
 import plan_wat from "./plan_wat.jpg";
 
@@ -7,22 +6,20 @@ function Home({ text }) {
   return (
     <div className="home">
       <div className="home_top">
-        <div className="home_top_about">O projekcies</div>
+        <div className="home_top_about">O projekcie</div>
       </div>
-      <div className="home_botton">
+      <div className="home_bottom">
         <div className="home_left">
-          <img className="plan" src={plan_wat} alt="logo" />
+          <img className="plan_wat" src={plan_wat} alt="logo" />
         </div>
-
         <div className="home_right">
           <div className="home_right_title">GEOPORTAL</div>
 
           <div className="home_right_subtitle">
             Strona internetowa z interaktywną mapą
           </div>
-          <Link to="/services">
-            <button className="home_right_button">START</button>
-          </Link>
+
+          <button className="home_right_button">START</button>
         </div>
       </div>
     </div>
