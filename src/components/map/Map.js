85ae6a5cc2189127642151fb43068fc38599f482
wiktoria250001 <149382337+wiktoria250001@users.noreply.github.com>
2024-05-30 22:15:40 +0200

    wip

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,9 +1,28 @@
-import React from "react";
-import { LayersControl, MapContainer, TileLayer } from "react-leaflet";
+import React, { useEffect } from "react";
+import {
+  LayersControl,
+  MapContainer,
+  TileLayer,
+  WMSTileLayer,
+} from "react-leaflet";
 import "./Map.css";
 import "leaflet/dist/leaflet.css";
+import axios from "axios";
+
+
+
+
 
 function Map() {
+useEffect{()=>{
+  console.log("aaa");
+  const getData=()=>{
+axios.get(link do naszych danych wfs, czyli adres z geoservera).then(dane=>{console.log(dane)})
+
+
+  }
+},[]};
+
   return (
     <div>
       <MapContainer center={[52.232222, 21.0]} zoom={6}>
@@ -16,6 +35,16 @@ function Map() {
           </LayersControl.BaseLayer>
           <LayersControl.BaseLayer name="OpenTopoMap">
             <TileLayer url="https://tile.opentopomap.org/{z}/{x}/{y}.png" />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="granice wojewodztw db">
+            <WMSTileLayer
+              layers="grance_wojewodztw_db"
+              url
+              wpisac
+              url
+              z
+              qgisa
+            />
           </LayersControl.BaseLayer>
         </LayersControl>
       </MapContainer>
